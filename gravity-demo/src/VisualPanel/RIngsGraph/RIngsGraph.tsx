739f3b73d9ffcbf68856f9d5e6c76a4a5356a2c5
Klaudia Mrozik <klaudia.butkiewicz09@gmail.com>
2022-09-15 08:03:57 +0200

    feat: get click position

--- gravity-demo/src/VisualPanel/RIngsGraph/RIngsGraph.tsx
+++ gravity-demo/src/VisualPanel/RIngsGraph/RIngsGraph.tsx
@@ -1,30 +1,46 @@
-import { createEffect, For } from "solid-js";
+import { createEffect, For, JSX } from "solid-js";
+import { createStore } from "solid-js/store";
 import { useRings } from "../../context/useRings";
 
 import { Ring } from "./Ring/Ring";
-import { StyledRingsGraphWrapper } from "./RingsGraph.styled";
+import { StyledRingsGraph, StyledRingsGraphWrapper } from "./RingsGraph.styled";
+
+type PointerPosition = {
+  x: number | null;
+  y: number | null;
+};
 
 export const RingsGraph = () => {
   const { state } = useRings();
 
-  const handleSvgClick = (e: MouseEvent) => {
-    // eslint-disable-next-line no-console
-    console.log(e);
+  const [pointerPosition, setPointerPosition] = createStore<PointerPosition>({
+    x: null,
+    y: null,
+  });
+
+  const handleSvgClick: JSX.EventHandler<HTMLDivElement, MouseEvent> = (e) => {
+    const parentDimensions = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - parentDimensions.left;
+    const y = e.clientY - parentDimensions.top;
+
+    setPointerPosition({ x, y });
   };
 
+  createEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(pointerPosition.x, pointerPosition.y);
+  });
+
   return (
-    <>
-      <StyledRingsGraphWrapper
-        viewBox="0 0 300 300"
-        onClick={(e) => handleSvgClick(e)}
-      >
+    <StyledRingsGraphWrapper onClick={handleSvgClick}>
+      <StyledRingsGraph viewBox="0 0 300 300">
         <g>
           <For each={state.rings}>
             {(ring) => <Ring radius={ring.radius} isActive={ring.active} />}
           </For>
           <circle cx="50%" cy="50%" r="3" />
         </g>
-      </StyledRingsGraphWrapper>
-    </>
+      </StyledRingsGraph>
+    </StyledRingsGraphWrapper>
   );
 };
--- gravity-demo/src/VisualPanel/RIngsGraph/RingsGraph.styled.tsx
+++ gravity-demo/src/VisualPanel/RIngsGraph/RingsGraph.styled.tsx
@@ -1,7 +1,12 @@
 import { styled } from "solid-styled-components";
 
-export const StyledRingsGraphWrapper = styled("svg")({
+export const StyledRingsGraphWrapper = styled("div")({
   maxWidth: "500px",
-  border: "1px solid #000",
   margin: "16px",
+});
+
+export const StyledRingsGraph = styled("svg")({
+  border: "1px solid #000",
+  width: "100%",
+  height: "100%",
 });
